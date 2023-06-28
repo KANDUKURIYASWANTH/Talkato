55f62d6ede4196b18ac9cd0469576d5514ec4e26
Kandukuri Yaswanth <kandukuriyaswanth27@gmail.com>
2023-06-28 19:38:05 +0530

    Approach changed
    The app previously build is not scalable and expandable

--- client/src/components/FlexBetween.jsx
+++
@@ -1,9 +0,0 @@
-import {Box} from "@mui/material";
-import {styled} from "@mui/system"
-const FlexBetween = styled(Box)({
-    display:"flex",
-    justifyContent:"space-around",
-    alignItems:"center",
-});
-
-export default FlexBetween;
