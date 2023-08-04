d7908db1aa0eec2d402b7bd1a70cfdad2368fa85
Kandukuri Yaswanth <kandukuriyaswanth27@gmail.com>
2023-08-04 10:34:05 +0530

    Confidential

--- client/src/utils/config.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'http://localhost:3000'
-export const UPLOAD_PRESENT="iwciwj3l"
-export const CLOUD_NAME="dq3ucyivu"
-export const fetchAPI_LINK="https://api.cloudinary.com/v1_1/dq3ucyivu/upload"
